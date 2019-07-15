0d932ab2f56f4af432cb72f1acdc1d61ecb4267b
Kamila Sieczkowska (KIO) <KIO@kmd.dk>
2019-07-15 16:32:36 +0200

    ready

---
+++ precache-manifest.eb62e974a1cdeba2e5e8297e9c3b8ca4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ab663ca8734cf0281d90f7f399cc89f",
+    "url": "/whereiam/index.html"
+  },
+  {
+    "revision": "2cc3a5ce2c63966ac5d4",
+    "url": "/whereiam/static/css/main.cc6c4c0d.chunk.css"
+  },
+  {
+    "revision": "400ca711028176baa6ab",
+    "url": "/whereiam/static/js/2.3d65e42c.chunk.js"
+  },
+  {
+    "revision": "2cc3a5ce2c63966ac5d4",
+    "url": "/whereiam/static/js/main.4b6f8293.chunk.js"
+  },
+  {
+    "revision": "83414f176f701b874e62",
+    "url": "/whereiam/static/js/runtime~main.28b6d859.js"
+  }
+]);
